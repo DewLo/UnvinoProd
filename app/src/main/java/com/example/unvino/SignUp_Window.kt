f5aba70f082b4c8253ed9b6a1abaaaff38660b7f
DewLo <dew.lo@yandex.ru>
2022-11-24 10:58:45 +0300

    Добавлено новое окно для регистрации

---
+++ app/src/main/java/com/example/unvino/SignUp_Window.kt
@@ -0,0 +1,11 @@
+package com.example.unvino
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUp_Window : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up_window)
+    }
+}
