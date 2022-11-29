9f07e508f5c6a999d8a6699803f60f62a8fdf088
Dew Lo <dew.lo@yandex.ru>
2022-11-29 11:43:48 +0300

    Add new windows

---
+++ app/src/main/java/com/example/unvino/SignIn_Window.kt
@@ -0,0 +1,11 @@
+package com.example.unvino
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignIn_Window : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in_window)
+    }
+}
